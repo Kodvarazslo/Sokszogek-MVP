5db7517d357ac2af744cfebb270a895c696d218f
Hullako Laszlo Antal <laszlo.hullako@gmail.com>
2019-10-14 18:34:45 +0200

    assda

--- Sokszogek-MVP/Form1.cs
+++ Sokszogek-MVP/Form1.cs
@@ -16,5 +16,10 @@ namespace Sokszogek_MVP
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
